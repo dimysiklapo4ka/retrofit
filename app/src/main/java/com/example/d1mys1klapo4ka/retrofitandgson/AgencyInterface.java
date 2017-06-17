e9b324be6cb5fda6d04303405e51e0b69d23a2d0
dimysiklapo4ka <dimysiklapo4ka@gmail.com>
2017-06-18 02:46:45 +0300

    xz kak polu4itb json

--- app/src/main/java/com/example/d1mys1klapo4ka/retrofitandgson/AgencyInterface.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/retrofitandgson/AgencyInterface.java
@@ -3,9 +3,13 @@ package com.example.d1mys1klapo4ka.retrofitandgson;
 import com.example.d1mys1klapo4ka.retrofitandgson.retrofitModel.Agency;
 import com.example.d1mys1klapo4ka.retrofitandgson.retrofitModel.AgencyList;
 
-import retrofit.Call;
-import retrofit.http.GET;
-import retrofit.http.Path;
+import java.util.List;
+
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 
 /**
  * Created by d1mys1klapo4ka on 17.06.2017.
@@ -13,30 +17,33 @@ import retrofit.http.Path;
 
 public interface AgencyInterface {
 
-    @GET("/agencies/{name}")
-    Call<AgencyList> getName(@Path("name") String name);
+    @GET("/api/v1/agencies")
+    Call<AgencyList> getAllData();
+
+    @GET("/api/v1/agencies")
+    Call<Agency> getName(@Path("name") String name);
 
     @GET("/agencies/{price}")
-    Call<AgencyList> agencyPrice(@Path("price") String price);
+    Call<Agency> agencyPrice(@Path("price") String price);
 
     @GET("/agencies/{phone}")
-    Call<AgencyList> agencyPhone(@Path("phone") Object phone);
+    Call<Agency> agencyPhone(@Path("phone") Object phone);
 
     @GET("/agencies/{address}")
-    Call<AgencyList> agencyAddress(@Path("address") Object address);
+    Call<Agency> agencyAddress(@Path("address") Object address);
 
     @GET("/agencies/{schrdule}")
-    Call<AgencyList> agencySchrdule(@Path("schrdule") Object schrdule);
+    Call<Agency> agencySchrdule(@Path("schrdule") Object schrdule);
 
     @GET("/agencies/{latitude}")
-    Call<AgencyList> agencyLatitude(@Path("latitude") Object latitude);
+    Call<Agency> agencyLatitude(@Path("latitude") Object latitude);
 
     @GET("/agencies/{longitude}")
-    Call<AgencyList> agencyLongitude(@Path("longitude") Object longitude);
+    Call<Agency> agencyLongitude(@Path("longitude") Object longitude);
 
     @GET("/agencies/{requisites}")
-    Call<AgencyList> agencyRequisites(@Path("requisites") Object requisites);
+    Call<Agency> agencyRequisites(@Path("requisites") Object requisites);
 
     @GET("/agencies/{credit_card}")
-    Call<AgencyList> agencyCreditCard(@Path("credit_card") Object creditCard);
+    Call<Agency> agencyCreditCard(@Path("credit_card") Object creditCard);
 }
--- app/src/main/java/com/example/d1mys1klapo4ka/retrofitandgson/MainActivity.java
+++ app/src/main/java/com/example/d1mys1klapo4ka/retrofitandgson/MainActivity.java
@@ -2,52 +2,61 @@ package com.example.d1mys1klapo4ka.retrofitandgson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
 
+import com.example.d1mys1klapo4ka.retrofitandgson.retrofitModel.Agency;
 import com.example.d1mys1klapo4ka.retrofitandgson.retrofitModel.AgencyList;
 
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.GsonConverterFactory;
-import retrofit.Response;
-import retrofit.Retrofit;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private String BASE_URL = "http://server.gojob.com.ua/api/v1/agencies";
+    private String BASE_URL = "http://server.gojob.com.ua/";
+    Retrofit retrofit;
+    AgencyInterface agencyInterface;
+    List<Agency> post;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Retrofit client = new Retrofit.Builder()
+        post = new ArrayList<>();
+
+        retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        AgencyInterface service = client.create(AgencyInterface.class);
-        Call<AgencyList> call = service.getName("Dimysik");
+        agencyInterface = retrofit.create(AgencyInterface.class);
 
-        call.enqueue(new Callback<AgencyList>() {
+//        try {
+//            Response response = agencyInterface.getAllData().execute();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
+        agencyInterface.getAllData().enqueue(new Callback<AgencyList>() {
             @Override
-            public void onResponse(Response<AgencyList> response, Retrofit retrofit) {
+            public void onResponse(Call<AgencyList> call, Response<AgencyList> response) {
 
-                if (response.isSuccess()) {
-                    // request successful (status code 200, 201)
-                    AgencyList result = response.body();
-                    Log.e("@@@@@@", "onResponse: AgencyList"+ result );
-                } else {
-                    //request not successful (like 400,401,403 etc)
-                    //Handle errors
+                Log.e("@@@@@", "onResponse: getAllData: " + response.body());
 
-                    Log.d("@@@@@@", "onResponse: Failed");
-                }
             }
 
             @Override
-            public void onFailure(Throwable t) {
+            public void onFailure(Call<AgencyList> call, Throwable t) {
 
             }
         });
